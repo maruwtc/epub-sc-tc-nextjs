8f261eab2107221e686a4777f00fb3a4072b5ce8
maruwtc <chriswong0621@gmail.com>
2025-04-01 22:04:20 +0800

    Integrate Analytics component into RootLayout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -38,6 +38,7 @@ export default function RootLayout({
           disableTransitionOnChange
         >
           {children}
+          <Analytics />
         </ThemeProvider>
       </body>
     </html>
